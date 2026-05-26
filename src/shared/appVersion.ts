9c98b8bf1b8571e06ff4f6f08f753a0f0101dff0
qubokjunior <qubokjunior@gmail.com>
2026-05-26 13:06:15 +0200

    m35 update app version

--- src/shared/appVersion.ts
+++ src/shared/appVersion.ts
@@ -1,4 +1,4 @@
 export const PROJECT_NAME = "qubok_evolve" as const;
-export const PROJECT_VERSION = "0.1.0-milestone.34" as const;
-export const PROJECT_MILESTONE = 34 as const;
-export const PROJECT_MILESTONE_LABEL = "m34" as const;
+export const PROJECT_VERSION = "0.1.0-milestone.35" as const;
+export const PROJECT_MILESTONE = 35 as const;
+export const PROJECT_MILESTONE_LABEL = "m35" as const;
