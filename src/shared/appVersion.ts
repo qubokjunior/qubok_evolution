8311632899a053314bda4f80432b80dd481788eb
qubokjunior <qubokjunior@gmail.com>
2026-05-26 15:22:43 +0200

    Bump project version to m38

--- src/shared/appVersion.ts
+++ src/shared/appVersion.ts
@@ -1,4 +1,4 @@
 export const PROJECT_NAME = "qubok_evolve" as const;
-export const PROJECT_VERSION = "0.1.0-milestone.37" as const;
-export const PROJECT_MILESTONE = 37 as const;
-export const PROJECT_MILESTONE_LABEL = "m37" as const;
+export const PROJECT_VERSION = "0.1.0-milestone.38" as const;
+export const PROJECT_MILESTONE = 38 as const;
+export const PROJECT_MILESTONE_LABEL = "m38" as const;
