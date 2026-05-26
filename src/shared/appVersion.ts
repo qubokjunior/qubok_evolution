ae4329665c0f4194b8b5a6b47ed70897a962af5c
qubokjunior <qubokjunior@gmail.com>
2026-05-26 17:09:10 +0200

    Update app version for m41

--- src/shared/appVersion.ts
+++ src/shared/appVersion.ts
@@ -1,4 +1,4 @@
-﻿export const PROJECT_NAME = "qubok_evolve" as const;
-export const PROJECT_VERSION = "0.1.0-milestone.40" as const;
-export const PROJECT_MILESTONE = 40 as const;
-export const PROJECT_MILESTONE_LABEL = "m40" as const;
+export const PROJECT_NAME = "qubok_evolve" as const;
+export const PROJECT_VERSION = "0.1.0-milestone.41" as const;
+export const PROJECT_MILESTONE = 41 as const;
+export const PROJECT_MILESTONE_LABEL = "m41" as const;
