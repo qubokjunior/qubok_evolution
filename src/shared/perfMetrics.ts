2c2898c6be9a2c892a7b3b72bfc3846f81d2543b
qubokjunior <qubokjunior@gmail.com>
2026-05-26 15:56:21 +0200

    Add field flow metrics

--- src/shared/perfMetrics.ts
+++ src/shared/perfMetrics.ts
@@ -2,7 +2,7 @@ export const PERF_METRICS_VERSION = "qubok_evolve.perf_metrics.v1" as const;
 
 export const PERF_METRIC_NAMES = [
   "fps", "frameMs", "renderMsPerFrame", "simMsPerTick", "entityCount", "aliveCount", "averageEnergy01", "tick",
-  "movementIntegratedCount", "movementAliveCount", "movementDistanceSum", "terrainMovementSampleCount", "terrainMovementCostSum", "terrainFrictionSum", "terrainDragSum",
+  "movementIntegratedCount", "movementAliveCount", "movementDistanceSum", "terrainMovementSampleCount", "terrainMovementCostSum", "terrainFrictionSum", "terrainDragSum", "fieldMovementSampleCount", "fieldFlowXSum", "fieldFlowYSum", "fieldFlowMagnitudeSum",
   "obstacleResponseMs", "obstacleResponseForces", "obstacleResponseHits", "obstacleResponseCellChecks", "obstacleResponseSkippedCells", "obstacleResponseLimitHits", "obstacleResponseBoundaryHits",
   "obstacleLifecycleEvents", "obstacleLifecyclePressure", "obstacleSpawnBlockedAttempts", "obstacleSpawnFallbacks", "obstacleSpawnFailures", "obstacleReproductionBlocked", "obstacleReproductionFailures", "obstacleResourceRespawns",
   "obstacleRenderMs", "obstacleRenderCellCount", "terrainRenderMs", "terrainRenderCellCount", "terrainRenderTruncated", "gridBuildMs", "neighborQueryMs", "neighborCandidates", "avgNeighborsPerAgent",
