6b071da51ab56d974300993a168d15c36cd86347
qubokjunior <qubokjunior@gmail.com>
2026-05-27 10:04:45 +0200

    Bump app version to M48

--- src/shared/appVersion.ts
+++ src/shared/appVersion.ts
@@ -1,4 +1,4 @@
 export const PROJECT_NAME = "qubok_evolve" as const;
-export const PROJECT_VERSION = "0.1.0-milestone.47" as const;
-export const PROJECT_MILESTONE = 47 as const;
-export const PROJECT_MILESTONE_LABEL = "m47" as const;
+export const PROJECT_VERSION = "0.1.0-milestone.48" as const;
+export const PROJECT_MILESTONE = 48 as const;
+export const PROJECT_MILESTONE_LABEL = "m48" as const;
